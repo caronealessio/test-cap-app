de20f60beec00b652422be81a54b4e4b48128850
--global <alessio.carone@innovatesapp.com>
2025-01-31 15:59:15 +0000

    SONO FINALMENTE RIUSCITO AD AGGANCIARE UN SERVIZIO SAP AD UNA APPLICAZIONE SAPUI5

--- cap-application/app/webapp/controller/Home.controller.js
+++ cap-application/app/webapp/controller/Home.controller.js
@@ -1,8 +1,9 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
+sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageBox"], (Controller, MessageBox) => {
   "use strict";
 
   return Controller.extend("appcap.appcap.controller.Home", {
-    onInit() {},
+    onInit: function () {
+    },
 
     onCallToInteger: function () {
       const value = "123.45"; // Valore di esempio da inviare al servizio
